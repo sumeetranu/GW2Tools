1dee5e2ef386d428bd34cac25278c18cf9365e73
sumeetranu <satwal1994@gmail.com>
2016-07-03 21:12:10 -0400

    Changed server.js for Azure

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
-var port = 3000;
+//var port = 3000;
+var port = process.env.port || 1337
 
 /*app.get('/', function(req, res){
 	res.send("Hello " + req.query.name + " from server.js!");
@@ -8,6 +9,17 @@ var port = 3000;
 
 app.use(express.static(__dirname + "/public"));
 
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
 
-app.listen(port);
-console.log('Server running on port', port);
+
+/*app.listen(port);
+console.log('Server running on port', port);*/
+
+var server = app.listen(port, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
